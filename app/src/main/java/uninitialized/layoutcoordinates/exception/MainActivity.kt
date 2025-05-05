30d6abea7be44e6afd60939c94d006c95ce244f8
Icks <whiny-exit-robin@duck.com>
2025-05-05 15:19:50 +0200

    simplify demo code

--- app/src/main/java/uninitialized/layoutcoordinates/exception/MainActivity.kt
+++ app/src/main/java/uninitialized/layoutcoordinates/exception/MainActivity.kt
@@ -5,24 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
-import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import uninitialized.layoutcoordinates.exception.ui.theme.UninitializedLayoutCoordinatesExceptionTheme
 
@@ -32,15 +28,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UninitializedLayoutCoordinatesExceptionTheme {
-                ScreenContent()
+                Screen()
             }
         }
     }
 }
 
-@OptIn(ExperimentalSharedTransitionApi::class)
+/**
+ * Press the button then rotate the device.
+ * -> IllegalArgumentException: Error: Uninitialized LayoutCoordinates
+ */
 @Composable
-internal fun ScreenContent() {
+@OptIn(ExperimentalSharedTransitionApi::class, ExperimentalMaterial3Api::class)
+internal fun Screen() {
     var contentShown by rememberSaveable { mutableStateOf(false) }
     SharedTransitionLayout {
         AnimatedContent(
@@ -52,49 +52,35 @@ internal fun ScreenContent() {
                     onClick = { contentShown = contentShown.not() }
                 ) {
                     Text(
-                        text = "Content: $shown",
+                        text = "Title",
                         modifier = Modifier.sharedElement(
                             sharedContentState = rememberSharedContentState(key = "SHARED_ELEMENT"),
                             animatedVisibilityScope = this@AnimatedContent
                         )
                     )
                 }
-            }
-            if (shown) {
-                AnimatedTitleContent(
-                    animatedVisibilityScope = this@AnimatedContent
-                )
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalSharedTransitionApi::class)
-@Composable
-fun SharedTransitionScope.AnimatedTitleContent(
-    animatedVisibilityScope: AnimatedContentScope
-) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            TopAppBar(
-                scrollBehavior = scrollBehavior,
-                title = {
-                    Text(
-                        text = "Content",
-                        modifier = Modifier.sharedElement(
-                            sharedContentState = rememberSharedContentState(key = "SHARED_ELEMENT"),
-                            animatedVisibilityScope = animatedVisibilityScope
+            } else {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = "Title",
+                                    modifier = Modifier.sharedElement(
+                                        sharedContentState = rememberSharedContentState(key = "SHARED_ELEMENT"),
+                                        animatedVisibilityScope = this
+                                    )
+                                )
+                            },
                         )
+                    }
+                ) { p ->
+                    Text(
+                        modifier = Modifier.padding(p),
+                        text = "Content"
                     )
-                },
-            )
+                }
+            }
         }
-    ) { p ->
-        Text(
-            modifier = Modifier.padding(p),
-            text = "Some Text"
-        )
     }
 }
